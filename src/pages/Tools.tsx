6502f1fc5c24066a82094b06451eeaffedd79016
BigHootNet <thetrigun@gmail.com>
2024-08-16 04:39:26 +0200

    v0.2

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import photo from '../assets/img/photo.png';
+import illu1 from '../assets/img/illu1.png';
+import illu2 from '../assets/img/illu2.png';
+import illu3 from '../assets/img/illu3.png';
 import '../styles/pages/Tools.scss';
+import GsapLogo from '../components/GsapLogo';
 
 const Tools: React.FC = () => {
   return (
     <div className="tools-page">
-      <h1>Tools Page</h1>
+      <div className="image-display">
+        <img src={photo} alt="Photo" className="photo-image" />
+      </div>
+      <div className="name_holder">
+          <h3>Fourmiguier</h3>
+          <h3>Tony</h3>
+      </div>
+      <div className="icon_holder">
+          <i className="devicon-typescript-plain"></i>
+          <i className="devicon-react-original"></i>
+          <i className="devicon-threejs-original"></i>
+          <GsapLogo/>
+          <i className="devicon-photoshop-plain"></i>
+          <i className="devicon-premierepro-plain"></i>
+      </div>
+      <img src={illu1} alt="Xenomorph sketch" className="illustration-pic1" />
+      <img src={illu2} alt="Ellen Ripley sketch" className="illustration-pic2" />
+      <img src={illu3} alt="Robotic Child sketch" className="illustration-pic3" />
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bh2/',  // Assurez-vous que ce chemin correspond à celui où vous déployez votre application
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-});
+})
